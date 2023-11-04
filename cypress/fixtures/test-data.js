abd3f4059abded1e0f7e638a3d682eae6d8964b1
Anna <anna.berejna@gmail.com>
2023-11-05 00:52:40 +0200

    add data

---
+++ cypress/fixtures/test-data.js
@@ -0,0 +1,18 @@
+const { faker } = require("@faker-js/faker");
+
+const randomUsername = faker.internet.userName();
+const randomEmail = faker.internet.email();
+const randomPassword = faker.internet.password();
+
+const randomBadUsername = faker.string.numeric(5);
+const randomBadEmail = faker.internet.userName();
+const randomBadPassword = faker.string.numeric(5);
+
+module.exports = {
+  randomUsername,
+  randomEmail,
+  randomPassword,
+  randomBadUsername,
+  randomBadEmail,
+  randomBadPassword
+};
